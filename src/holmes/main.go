bccea992caa19184bc0463027b7e68391ff1a861
周乐钦 <leqinzhou@anjuke.com>
2013-07-08 11:03:43 +0800

    main.go

---
+++ src/holmes/main.go
@@ -0,0 +1,16 @@
+package main
+
+var holmesConf HolmesConfig
+
+func main() {
+	confFile := "holmes.conf"
+	holmesConf = LoadConfig(confFile)
+	InitRedisConf(&holmesConf)
+	c := make(chan int, 1)
+	go StageLog(c, &holmesConf)
+	go Filter(c)
+	go Export(c, &holmesConf)
+	for i := 0; i < 3; i++ { //waiting all goroutines to finish
+		<-c
+	}
+}
