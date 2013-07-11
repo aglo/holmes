c2bd725c1911c67ce55f54db2680197875c91138
周乐钦 <leqinzhou@anjuke.com>
2013-07-11 10:46:12 +0800

    exporter.go:Delete feature:now the function do nothing

--- src/holmes/exporter.go
+++ src/holmes/exporter.go
@@ -1,11 +1,5 @@
 package main
 
 func Export(c chan int, holmesConfig *HolmesConfig) {
-	for {
-		//BlockListPop("accesslog_yes", 1)
-		//BlockListPop("accesslog_no", 1)
-		BlockListRightPop("accesslog", 0)
-		//guid:=BlockListPop("guid",accesslog.GUID)
-	}
 	c <- 1
 }
