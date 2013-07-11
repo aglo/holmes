a434136af8eff14f786cd2157e65d74aa2a0dc72
周乐钦 <leqinzhou@anjuke.com>
2013-07-11 10:17:48 +0800

    Format:change spaces to tab

--- src/holmes/filter.go
+++ src/holmes/filter.go
@@ -1,47 +1,47 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
 const (
-    YES = iota // is a human
-    NO         // is not a human
-    UNKNOWN
+	YES = iota // is a human
+	NO         // is not a human
+	UNKNOWN
 )
 
 func Filter(c chan int) {
-    for {
-        _, accesslogLine := BlockListRightPop("accesslog", 0)
+	for {
+		_, accesslogLine := BlockListRightPop("accesslog", 0)
 
-        accesslog := GetLog(accesslogLine)
-        fmt.Printf("filter==>%s\n", accesslogLine)
+		accesslog := GetLog(accesslogLine)
+		fmt.Printf("filter==>%s\n", accesslogLine)
 
-        filterResult := DoFilter(accesslog)
-        if filterResult == YES {
-            ListLeftPush("accesslog_yes", accesslogLine)
-        } else if filterResult == NO {
-            ListLeftPush("accesslog_no", accesslogLine)
-        } else {
-            ListLeftPush("accesslog_unkown", accesslogLine)
-        }
-    }
-    c <- 1
+		filterResult := DoFilter(accesslog)
+		if filterResult == YES {
+			ListLeftPush("accesslog_yes", accesslogLine)
+		} else if filterResult == NO {
+			ListLeftPush("accesslog_no", accesslogLine)
+		} else {
+			ListLeftPush("accesslog_unkown", accesslogLine)
+		}
+	}
+	c <- 1
 
 }
 
 func DoFilter(accesslog AccessLog) int {
-    return GUIDFilter(accesslog)
+	return GUIDFilter(accesslog)
 }
 
 func GUIDFilter(accesslog AccessLog) int {
-    if accesslog.GUID == "-" {
-        return NO
-    } else {
-        ListLeftPush("guid", accesslog.GUID)
-        ListLeftPush(accesslog.GUID, "----"+accesslog.Referer)
-        uri := accesslog.LogTimeString() + "==>" + accesslog.RequestURI
-        ListLeftPush(accesslog.GUID, uri)
-        return YES
-    }
+	if accesslog.GUID == "-" {
+		return NO
+	} else {
+		ListLeftPush("guid", accesslog.GUID)
+		ListLeftPush(accesslog.GUID, "----"+accesslog.Referer)
+		uri := accesslog.LogTimeString() + "==>" + accesslog.RequestURI
+		ListLeftPush(accesslog.GUID, uri)
+		return YES
+	}
 }
--- src/holmes/main.go
+++ src/holmes/main.go
@@ -3,14 +3,14 @@ package main
 var holmesConf HolmesConfig
 
 func main() {
-    confFile := "holmes.conf"
-    holmesConf = LoadConfig(confFile)
-    InitRedisConf(&holmesConf)
-    c := make(chan int, 1)
-    go StageLog(c, &holmesConf)
-    go Filter(c)
-    go Export(c, &holmesConf)
-    for i := 0; i < 3; i++ { // waiting all goroutines to finish
-        <-c
-    }
+	confFile := "holmes.conf"
+	holmesConf = LoadConfig(confFile)
+	InitRedisConf(&holmesConf)
+	c := make(chan int, 1)
+	go StageLog(c, &holmesConf)
+	go Filter(c)
+	go Export(c, &holmesConf)
+	for i := 0; i < 3; i++ { // waiting all goroutines to finish
+		<-c
+	}
 }
