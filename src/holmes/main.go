356115cb58e902ce6db313e5cd596060991e3ade
周乐钦 <leqinzhou@anjuke.com>
2013-07-15 17:40:36 +0800

    src/holmes/main.go添加多线程支持，初始化UserAgent正则表达式

--- src/holmes/main.go
+++ src/holmes/main.go
@@ -1,11 +1,22 @@
 package main
 
+import (
+	"runtime"
+)
+
 var holmesConf HolmesConfig
+
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
 
 func main() {
 	confFile := "holmes.conf"
+	ua_pattern_file := "../data/user_agent_pattern.json"
 	holmesConf = LoadConfig(confFile)
 	InitRedisConf(&holmesConf)
+	InitUAParsers(ua_pattern_file)
+	
 	c := make(chan int, 3)
 	go StageLog(c, &holmesConf)
 	go Filter(c)
