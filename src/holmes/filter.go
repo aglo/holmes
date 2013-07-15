3051e98f9ad632d20ec103c368fad01c12edc0ce
周乐钦 <leqinzhou@anjuke.com>
2013-07-15 18:10:23 +0800

    Conflicts: 	src/holmes/filter.go

--- src/holmes/filter.go
+++ src/holmes/filter.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"strings"
+	//"net"
+	//"net/http"
+	"regexp"
 	"time"
 )
 
@@ -34,43 +37,40 @@ func Filter(c chan int) {
 		i++
 
 		filterResult = DoFilter(redisConn, accesslog)
+
+		//  these should done in filter function
+		//
 		if filterResult == YES {
 			redisConn.ListLeftPush("accesslog_yes", accesslogLine)
-		} else if filterResult == NO {
-			redisConn.ListLeftPush("accesslog_no", accesslogLine)
-		} else {
-			redisConn.ListLeftPush("accesslog_unkown", accesslogLine)
 		}
+		//else if filterResult == NO {
+		//	redisConn.ListLeftPush("accesslog_no", accesslogLine)
+		//} else {
+		//	redisConn.ListLeftPush("accesslog_unkown", accesslogLine)
+		//}
 	}
 	c <- 1
 }
 
 func DoFilter(redisConn RedisConn, accesslog AccessLog) int {
-	FilterFlag := UNKNOWN
-	FilterFlag = UserAgentFilter(redisConn, accesslog)
-	////FilterFlag = GUIDFilter(accesslog)
-	//FilterFlag = IPFilter(accesslog)
-	return FilterFlag
+	return URIFilter(redisConn, accesslog)
 }
 
-func GUIDFilter(redisConn RedisConn, accesslog AccessLog) int {
-	if accesslog.GUID == "-" {
-		return NO
+func URIFilter(redisConn RedisConn, accesslog AccessLog) int {
+	if matched, err := regexp.MatchString("^/prop/view", accesslog.RequestURI); err == nil && matched {
+		return HttpCodeFilter(redisConn, accesslog)
 	} else {
-		redisConn.ListLeftPush("guid", accesslog.GUID)
-		redisConn.ListLeftPush(accesslog.GUID, "----"+accesslog.Referer)
-		uri := accesslog.LogTimeString() + "==>" + accesslog.RequestURI
-		redisConn.ListLeftPush(accesslog.GUID, uri)
-		return YES
+		//TODO Analysis(redisConn,accesslog)
+		return UNKNOWN
 	}
 }
 
-func IPFilter(redisConn RedisConn, accesslog AccessLog) int {
-	redisConn.SetAdd("ip", accesslog.RemoteAddr)
-	redisConn.ListLeftPush(accesslog.RemoteAddr, "----"+accesslog.Referer)
-	uri := accesslog.LogTimeString() + "==>" + accesslog.RequestURI
-	redisConn.ListLeftPush(accesslog.RemoteAddr, uri)
-	return UNKNOWN
+func HttpCodeFilter(redisConn RedisConn, accesslog AccessLog) int {
+	if matched, err := regexp.MatchString("^2", accesslog.HttpCode); err == nil && matched {
+		return UserAgentFilter(redisConn, accesslog)
+	} else {
+		return UNKNOWN
+	}
 }
 
 func UserAgentFilter(redisConn RedisConn, accesslog AccessLog) int {
@@ -88,4 +88,75 @@ func UserAgentFilter(redisConn RedisConn, accesslog AccessLog) int {
 		redisConn.SetAdd("ua", uaFamily)
 		return YES
 	}
+
+	//////////// get UA type from website
+	//
+	//_, err := http.Get("http://www.useragentstring.com/?usa=" + accesslog.UserAgent + "&getText=all")
+	//if err != nil {
+	//	fmt.Println(err)
+	//} else {
+	//	//	fmt.Println("success", res)
+	//}
+
+	////////////  DNS reverse lookup
+	//
+	//if matched,err := regexp.MatchString("[S|s]pider",accesslog.UserAgent) ; err != nil || !matched{
+	//    return UNKNOWN
+	//} else {
+	//    ans , err1 := net.LookupAddr(accesslog.RemoteAddr)
+	//    if err1 != nil{
+	//        fmt.Println("Failed",accesslog.UserAgent,"+", accesslog.RemoteAddr,err1)
+	//    } else {
+	//        fmt.Println("Successful",accesslog.UserAgent,"+",accesslog.RemoteAddr,"-->",ans)
+	//    }
+	//    return UNKNOWN
+	//}
 }
+
+func WhiteIpFilter(redisConn RedisConn, accesslog AccessLog) int {
+	if true {
+		//TODO if accesslog . RemoteAddr == (check whether in the whitelist)
+
+		return YES
+	} else {
+		return UNKNOWN
+	}
+}
+
+func AddWatchingList(redisConn RedisConn, accesslog AccessLog) {
+	redisConn.ListLeftPush("WatchingList", accesslog.String())
+}
+
+func DelWatchingList(redisConn RedisConn, accesslog AccessLog) {
+	//redisConn.ListRigthPop("WatchingList", accesslog.String())
+	//
+	//   TODO pop specific record
+}
+
+func AddWhiteList(redisConn RedisConn, accesslog AccessLog) {
+	redisConn.ListLeftPush("WhiteList", accesslog.String())
+}
+
+func AddIgnoreList(redisConn RedisConn, accesslog AccessLog) {
+	redisConn.ListLeftPush("IgnoreList", accesslog.String())
+}
+
+//func GUIDFilter(redisConn RedisConn, accesslog AccessLog) int {
+//	if accesslog.GUID == "-" {
+//		return NO
+//	} else {
+//		redisConn.ListLeftPush("guid", accesslog.GUID)
+//		redisConn.ListLeftPush(accesslog.GUID, "----"+accesslog.Referer)
+//		uri := accesslog.LogTimeString() + "==>" + accesslog.RequestURI
+//		redisConn.ListLeftPush(accesslog.GUID, uri)
+//		return YES
+//	}
+//}
+//
+//func IPFilter(redisConn RedisConn, accesslog AccessLog) int {
+//	redisConn.SetAdd("ip", accesslog.RemoteAddr)
+//	redisConn.ListLeftPush(accesslog.RemoteAddr, "----"+accesslog.Referer)
+//	uri := accesslog.LogTimeString() + "==>" + accesslog.RequestURI
+//	redisConn.ListLeftPush(accesslog.RemoteAddr, uri)
+//	return UNKNOWN
+//}
