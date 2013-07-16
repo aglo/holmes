e2bd2b3fc7f88e8708bd085537f69a5377af4438
周乐钦 <leqinzhou@anjuke.com>
2013-07-16 16:44:07 +0800

    src/holmes/redis.go添加删除hash表中字段的方法

--- src/holmes/redis.go
+++ src/holmes/redis.go
@@ -225,6 +225,18 @@ func (redisConn *RedisConn) HashGet(ht string, field string) string {
 	return result
 }
 
+func (redisConn *RedisConn) HashDel(ht string, field string) int64 {
+	var result int64
+	if redisConn.c != nil {
+		r, err := redisConn.c.Do("HDEL", ht, field)
+		if err != nil {
+			panic(err)
+		}
+		result = r.(int64)
+	}
+	return result
+}
+
 func (redisConn *RedisConn) HashIncrby(ht string, field string, increment int) int64 {
 	var result int64
 	if redisConn.c != nil {
